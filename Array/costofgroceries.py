d87718cb5fd709eb9daf504a3023a6eb40b732d3
nishadanil <anilnishad19799@gmail.com>
2023-10-20 23:54:07 +0530

    try to solve cost of groceries problem

---
+++ Array/costofgroceries.py
@@ -0,0 +1,7 @@
+testcase = int(input())
+
+for i in range(testcase):
+    no_of_item ,freshnessofitem = input().split()
+    freshnessofitemarray = input().split()
+    costofeachitem = input().split()
+    
